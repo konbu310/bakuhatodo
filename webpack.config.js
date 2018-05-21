6cb1bbfab31d270016708555be5f75e1f9e33bd9
yuya <yuyas203@gmail.com>
2018-05-21 14:31:14 +0900

    これがホントのproduction

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
-const MODE = 'development';
-const enabledSourceMap = MODE === 'production';
+const MODE = 'production';
+const enabledSourceMap = MODE === 'development';
 
 module.exports = {
   mode: MODE,
